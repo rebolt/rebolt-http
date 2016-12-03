6e2d8db0c7b7368d4df7a767c660a5a484a07798
ikenn <ikenn@nexon.co.kr>
2016-12-03 17:54:20 +0900

    + SyncFactory, post 테스트코드

--- src/test/java/Test_SyncFactory.java
+++ src/test/java/Test_SyncFactory.java
@@ -15,6 +15,8 @@
  */
 
 import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.net.MediaType;
+import io.rebolt.http.HttpMethod;
 import io.rebolt.http.HttpRequest;
 import io.rebolt.http.HttpResponse;
 import io.rebolt.http.HttpStatus;
@@ -29,7 +31,7 @@ public final class Test_SyncFactory {
   @Test
   public void test_get() {
     SyncFactory factory = new SyncFactory(OkHttp3Engine.class);
-    HttpRequest request = HttpRequest.create().url("https://stamp.mp.nexon.com/ver");
+    HttpRequest request = HttpRequest.create().url("https://stamp.mp.nexon.com").path("/ver");
     HttpResponse<String> response = factory.invoke(request);
 
     assertTrue(response.getStatus().equals(HttpStatus.OK_200));
@@ -39,10 +41,26 @@ public final class Test_SyncFactory {
   @Test
   public void test_get2() {
     SyncFactory factory = new SyncFactory();
-    HttpRequest request = HttpRequest.create(JsonNode.class).url("https://m-api.nexon.com/signin.nx");
+    HttpRequest request = HttpRequest.create(JsonNode.class).url("https://m-api.nexon.com").path("/signin.nx");
     HttpResponse<JsonNode> response = factory.invoke(request);
 
     assertTrue(response.getStatus().equals(HttpStatus.NOT_FOUND_404));
     assertTrue(response.getBody().get("errorCode").asInt() == -2);
+  }
+
+  @Test
+  public void test_post() {
+    SyncFactory factory = new SyncFactory();
+    HttpRequest request = HttpRequest.create(String.class, JsonNode.class)
+        .url("https://m-api.nexon.com")
+        .method(HttpMethod.Post)
+        .path("/svr/validateNPToken.nx")
+        .contentType(MediaType.JSON_UTF_8)
+        .accept(MediaType.JSON_UTF_8)
+        .body("{\"svcID\":1003,\"npSN\":\"10030000000001132\",\"npToken\":\"TOTmgZqnjLwSBD0kFeEUpRsvb008o76O7W5SEh9Ud651UPa73lFN\"}");
+    HttpResponse<JsonNode> response = factory.invoke(request);
+
+    assertTrue(response.getStatus().equals(HttpStatus.OK_200));
+    assertTrue(response.getBody().get("errorCode").asInt() == -13);
   }
 }
